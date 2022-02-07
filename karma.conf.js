be0de8a2edd9b479aaeebba81a1389da1092ba93
geraldojunor0309 <geraldopgomesjr@gmail.com>
2022-02-07 01:18:36 -0300

    subindo LH-Angular

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/lh-angular'),
+      dir: require('path').join(__dirname, './coverage/lg1-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
